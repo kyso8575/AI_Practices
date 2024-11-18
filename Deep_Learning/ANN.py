52b8aee200c30cfbf1b418f071307e593b8a0a0f
kyso8575 <kyso8575@gmail.com>
2024-11-18 17:18:38 +0900

    Edit ANN Practice

--- Deep_Learning/ANN.py
+++ Deep_Learning/ANN.py
@@ -6,19 +6,88 @@ import torchvision.transforms as transforms
 import ssl
 
 
+# ssl 인증 무시
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-# 데이터셋 전처리
+# Data Preprocessing Pipeline / Data to tensor & 0.5 Mean, 0.5 Std Nomralization
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5,), (0.5,))
 ])
 
 
-# MNIST 데이터셋 로드
+# Use MNIST dataset
 trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
 testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
+
+
+sample_data, sample_label = trainset[0]
+
+print(f'Sample data shape: {sample_data}') # torch.Size 1, 28, 28
+print(f'Sample label: {sample_label}') # 5
+
+# Define ANN model
+class SimpleANN(nn.Module):
+    def __init__(self):
+        super(SimpleANN, self).__init__()
+        self.fc1 = nn.Linear(28 * 28, 128)  # 128 ,64 자주 쓰이는 숫자
+        self.fc2 = nn.Linear(128, 64)       
+        self.fc3 = nn.Linear(64, 10)        
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)  # 입력 이미지를 1차원 벡터로 변환
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+# 모델 초기화
+model = SimpleANN()
+
+# 손실 함수와 최적화 알고리즘 정의
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+# 모델 학습
+for epoch in range(10):  # 10 에포크 동안 학습
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+
+        # 기울기 초기화
+        optimizer.zero_grad()
+
+        # 순전파 + 역전파 + 최적화
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # 손실 출력
+        running_loss += loss.item()
+        if i % 100 == 99:  # 매 100 미니배치마다 출력
+            print(f'[Epoch {epoch + 1}, Batch {i + 1}] loss: {running_loss / 100:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
+
+
+
+correct = 0
+total = 0
+
+with torch.no_grad(): # 평가에는 기울기 계산이 필요하지 않음
+    for data in testloader:
+        images, labels = data
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the 10000 test images: {100 * correct / total:.2f}%')
+# Accuracy of the network on the 10000 test images: 97.00%
