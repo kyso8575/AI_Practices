306f7a384f2ba97d226ef99e574abbe144d42b8d
kyso8575 <kyso8575@gmail.com>
2024-11-18 15:24:42 +0900

    Improved RAG practice

--- RAG/RAG_practice.py
+++ RAG/RAG_practice.py
@@ -1,6 +1,7 @@
 # 환경 변수
 from dotenv import load_dotenv
 import os
+import datetime
 
 # AI 모델
 from langchain_openai import ChatOpenAI
@@ -25,13 +26,18 @@ model = ChatOpenAI(model="gpt-4o-mini")
 embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
 
 
+prompt = 'prompt3'
+prompt_file_path = f'./prompts/{prompt}.txt'
+
+# 텍스트 파일 읽기
+with open(prompt_file_path, 'r', encoding='utf-8') as file:
+    prompt_content = file.read()
+
 pdf_file_path ='./docs/인공지능산업최신동향 2024년 11월.pdf'
 
 loader = PyPDFLoader(pdf_file_path)
 
 docs = loader.load()
-
-
 
 recursive_text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=100,
@@ -46,17 +52,23 @@ vector_store = FAISS.from_documents(documents=splits, embedding=embeddings)
 
 retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 1})
 
-contextual_prompt = ChatPromptTemplate.from_messages([
-    ("system", "Answer the question using only the following context."),
-    ("user", "Context: {context}\\n\\nQuestion: {question}")
-])
 
+messages = [
+    ("system", line.split(": ", 1)[1])
+    for line in prompt_content.splitlines()
+    if line.startswith("system:") or line.startswith("user:")
+]
+
+
+# ChatPromptTemplate 생성
+contextual_prompt = ChatPromptTemplate.from_messages(messages)
 
 class DebugPassThrough(RunnablePassthrough):
     def invoke(self, *args, **kwargs):
         output = super().invoke(*args, **kwargs)
         print("Debug Output:", output)
         return output
+        
 # 문서 리스트를 텍스트로 변환하는 단계 추가
 class ContextToText(RunnablePassthrough):
     def invoke(self, inputs, config=None, **kwargs):  # config 인수 추가
@@ -71,12 +83,29 @@ rag_chain_debug = {
 }  | DebugPassThrough() | ContextToText()|   contextual_prompt | model
 
 
-while True: 
-	print("========================")
-	query = input("질문을 입력하세요: ")
-	response = rag_chain_debug.invoke(query)
-	print("Final Response:")
-	print(response.content)
+result_dir = './Results'
+timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")  # 타임스탬프 형식: 20231118_152345
+file_name = f"{prompt}_{timestamp}.txt"  # 예시 파일 이름: AI_Trend_Analysis_20231118_152345.txt
 
+# 파일 경로 설정
+file_path = os.path.join(result_dir, file_name)
 
-# 미국  민권위원회가 2024년 9월 19일에 발간한 보고서에 대해 설명해줘
+# 질문과 응답을 저장할 파일 열기
+with open(file_path, 'w') as file:
+    while True:
+        print("========================")
+        query = input("질문을 입력하세요    : ")
+        
+        # RAG 체인에서 응답을 받음
+        response = rag_chain_debug.invoke(query)
+        
+        # 질문과 응답을 출력
+        print("Final Response:")
+        print(response.content)
+        
+        # 질문과 응답을 파일에 기록
+        file.write(f"Question: {query}\n")
+        file.write(f"Response: {response.content}\n")
+        file.write("========================\n")
+
+# 챗봇이 보통 최신 정보는 학습이 되어있지 않기 때문에 RAG를 통해서, 정보를 제공해주고, 최신 정보에 대한 답을 얻을 수 있기 때문에 RAG가 중요하다.
