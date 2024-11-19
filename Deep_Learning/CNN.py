ee927d7aa32c2494ce9e1ea0e6754f416eacad58
kyso8575 <kyso8575@gmail.com>
2024-11-19 18:58:51 +0900

    Imrpoved CNN practice

--- Deep_Learning/CNN.py
+++ Deep_Learning/CNN.py
@@ -10,10 +10,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 # RGB 각각의 채널의 평균 std 를 0.5로 맞추기 (R mean, G mean , B mean) , (R std, G std, B std) 
 transform = transforms.Compose([
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
-
 
 # CIFAR-10 데이터셋 로드
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
@@ -21,7 +21,6 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
-
 
 class SimpleCNN(nn.Module):
     def __init__(self):
@@ -41,29 +40,52 @@ class SimpleCNN(nn.Module):
         return x
 
 
+class CNNWithDropoutAndBatchNorm(nn.Module):
+    def __init__(self):
+        super(CNNWithDropoutAndBatchNorm, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.fc1 = nn.Linear(128 * 56 * 56, 256)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(256, 10)
+
+    def forward(self, x):
+        x = torch.relu(self.bn1(self.conv1(x)))
+        x = torch.max_pool2d(x, 2)
+        x = torch.relu(self.bn2(self.conv2(x)))
+        x = torch.max_pool2d(x, 2)
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+
 # 모델 초기화
-model = SimpleCNN()
+model = CNNWithDropoutAndBatchNorm()
 
 # 손실 함수와 최적화 알고리즘 정의
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# 모델 학습
-for epoch in range(10):  # 10 에포크 동안 학습
+num_epochs = 10
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
+
+for epoch in range(num_epochs):
+    model.train()
     running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
+    for i, (inputs, labels) in enumerate(trainloader):
+        inputs, labels = inputs.to(device), labels.to(device)
 
-        # 기울기 초기화
         optimizer.zero_grad()
-
-        # 순전파 + 역전파 + 최적화
         outputs = model(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
 
-        # 손실 출력
         running_loss += loss.item()
         if i % 100 == 99:  # 매 100 미니배치마다 출력
             print(f'[Epoch {epoch + 1}, Batch {i + 1}] loss: {running_loss / 100:.3f}')
@@ -72,15 +94,16 @@ for epoch in range(10):  # 10 에포크 동안 학습
 print('Finished Training')
 
 
+
+model.eval()
 correct = 0
 total = 0
 with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = model(images)
+    for inputs, labels in testloader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
 print(f'Accuracy of the network on the 10000 test images: {100 * correct / total:.2f}%')
-# Accuracy of the network on the 10000 test images: 74.24%
